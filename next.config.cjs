024c978b0e39b224f8015868a17ad37af755569d
heinhtetaung19 <heinhtetaung1952@gmail.com>
2023-05-04 13:00:07 +0630

    updated .gitignore

---
+++ next.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    globalStylePaths: ["./styles/globals.css"],
+  }
+}
+
+module.exports = nextConfig
